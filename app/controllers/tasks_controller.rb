cc118144b9d215065b0307054db1464ce4ddac16
Daphnee Duportal <daphnee.duportal@gmail.com>
2019-11-05 15:06:35 +0100

    idem

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,26 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.all
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @tasks = Task.new(task_params)
+
+    @tasks.save
+    redirect_to task_path(@tasks)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # ALL
   get 'tasks', to: 'tasks#index'
 
-  # READ
+  #CREATE
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+
+   # READ
   get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
